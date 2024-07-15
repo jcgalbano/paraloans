8d2401624dd3d54980d5a9411149cb65bbb6795c
JC Albano <jcgalbano@gmail.com>
2024-07-15 15:57:29 +0800

    Fonts: add proxima-nova and calluna

---
+++ web/src/app/head.tsx
@@ -0,0 +1,11 @@
+import NextHead from "next/head";
+
+const Head = () => {
+  return (
+    <>
+      <link rel="stylesheet" href="https://use.typekit.net/qrl5ogo.css" />
+    </>
+  );
+};
+
+export default Head;
--- web/tailwind.config.ts
+++ web/tailwind.config.ts
@@ -7,6 +7,11 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    fontFamily: {
+      "proxima-nova": ["proxima-nova", "sans-serif"],
+      "proxima-nova-condensed": ["proxima-nova-condensed", "sans-serif"],
+      calluna: ["calluna", "serif"],
+    },
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
