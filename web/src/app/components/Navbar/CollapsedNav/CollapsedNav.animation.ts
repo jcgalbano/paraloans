287fca758669c41675c471675d1e48122ac720b2
JC Albano <jcgalbano@gmail.com>
2024-07-17 01:34:27 +0800

    Fix: correct CollapsedNav variants

--- web/src/app/components/Navbar/CollapsedNav/CollapsedNav.animation.ts
+++ web/src/app/components/Navbar/CollapsedNav/CollapsedNav.animation.ts
@@ -1,4 +1,4 @@
 export const collapsedNavVariants = {
-  hidden: { y: 40, opacity: 0, z: 0, visibility: "hidden" },
-  visible: { y: 60, opacity: 1, z: 2, visibility: "visible" },
+  hidden: { y: 40, opacity: 0, visibility: "hidden" as "hidden" },
+  visible: { y: 60, opacity: 1, visibility: "visible" as "visible" },
 };
