201d6048806e98a5e685ed9de3ebc05d7af74ae0
JC Albano <jcgalbano@gmail.com>
2024-07-17 17:30:37 +0800

    Refactor: adjust altnav spacing on mobile

--- web/src/app/components/NavbarAlt/NavbarAlt.tsx
+++ web/src/app/components/NavbarAlt/NavbarAlt.tsx
@@ -9,15 +9,15 @@ import PhoneIcon from "/public/phone-icon.svg";
 export const NavbarAlt = () => {
   return (
     <nav className="w-full fixed z-4">
-      <div className="flex justify-between items-center p-0 xs:p-5">
+      <div className="flex justify-between items-center pt-5 pl-1 pr-1 pb-5 xs:p-5">
         <div className="block 2sm:hidden">
           <Navlogo type="main" />
         </div>
         <div className="hidden 2sm:block">
           <Navlogo type="alt" />
         </div>
-        <div className="flex space-x-5">
-          <div className="flex items-center xs:space-x-3">
+        <div className="flex space-x-1 xs:space-x-3">
+          <div className="flex items-center space-x-0 xs:space-x-3">
             <Image height={16} width={16} src={PhoneIcon} alt="phone icon" />{" "}
             <span className="font-proximaNova text-base text-neutral-700">
               1300 755 494
