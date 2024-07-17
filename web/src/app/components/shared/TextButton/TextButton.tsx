a97c971759b793459029cba55c581fc95731d0e1
JC Albano <jcgalbano@gmail.com>
2024-07-17 17:49:47 +0800

    Refactor: remove sass for TextButton

--- web/src/app/components/shared/TextButton/TextButton.tsx
+++ web/src/app/components/shared/TextButton/TextButton.tsx
@@ -1,9 +1,13 @@
 import { TextButtonProps } from "./TextButton.types";
-import styles from "./TextButton.module.scss";
 
 export const TextButton = ({ content, size, callback }: TextButtonProps) => {
   return (
-    <button className={styles.txtBtn} onClick={callback}>
+    <button
+      className="bg-general-white font-proximaNova text-base
+    text-primary-500 uppercase rounded-rounded5px p-lgButton
+    hover:bg-primary-50"
+      onClick={callback}
+    >
       {content}
     </button>
   );
