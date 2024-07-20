f6b8f5bc35edcdecc607fc7ebb7b233c0fb4538b
JC Albano <jcgalbano@gmail.com>
2024-07-21 05:30:06 +0800

    Feat(component): add FormPhoneField

--- web/src/app/(loan-form)/contact-details/page.tsx
+++ web/src/app/(loan-form)/contact-details/page.tsx
@@ -1,8 +1,27 @@
 "use client";
+
 import { NextPage } from "next";
 
+import { FormHeadliner } from "@/app/components/FormHeadliner";
+import { FormPhoneField } from "@/app/components/FormPhoneField";
+import { FormTextField } from "@/app/components/FormTextField";
+
 const ContactDetails: NextPage = () => {
-  return <div>Contact Details Form Lives Here</div>;
+  const headline: string = "Contact Details";
+  const subheading: string = "How can we reach you?";
+
+  return (
+    <form className="w-auto sm:w-[500px]">
+      <FormHeadliner text={headline} subtext={subheading} />
+      <FormTextField field="email" label="Email" required={true} />
+      <FormPhoneField
+        field="phoneNumber"
+        label="Mobile Number"
+        countryFormat="au"
+        required={true}
+      />
+    </form>
+  );
 };
 
 export default ContactDetails;
