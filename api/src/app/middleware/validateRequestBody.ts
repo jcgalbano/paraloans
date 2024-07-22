c62ce72ca27cbdc94c2b45c762deca2914f66395
JC Albano <jcgalbano@gmail.com>
2024-07-23 02:08:00 +0800

    Feat(middleware): sanitize input using Validator

--- api/src/app/middleware/validateRequestBody.ts
+++ api/src/app/middleware/validateRequestBody.ts
@@ -1,12 +1,15 @@
 import { Request, Response, NextFunction } from "express";
 
 import loanApplicationSchema from "../schemas/loanApplicationSchema";
+import { sanitizeInput } from "../util/sanitizer";
 
 const validateRequestBody = (
   req: Request,
   res: Response,
   next: NextFunction
 ) => {
+  req.body = sanitizeInput(req.body);
+
   const { error } = loanApplicationSchema.validate(req.body);
   if (error) {
     return res.status(400).json({ error: error.details[0].message });
